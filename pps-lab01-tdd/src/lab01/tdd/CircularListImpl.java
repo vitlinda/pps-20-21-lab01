57a0338dc06ffb321a3338d61cb69c223558b8a1
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-03-04 22:14:59 +0100

    es2 - listIsEmptyBeforeAnyAdd test passed

--- pps-lab01-tdd/src/lab01/tdd/CircularListImpl.java
+++ pps-lab01-tdd/src/lab01/tdd/CircularListImpl.java
@@ -19,7 +19,7 @@ public class CircularListImpl implements CircularList {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return circularList.isEmpty();
     }
 
     @Override
--- pps-lab01-tdd/test/lab01/tdd/CircularListTest.java
+++ pps-lab01-tdd/test/lab01/tdd/CircularListTest.java
@@ -1,14 +1,20 @@
 package lab01.tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
-    private final CircularListImpl circularList = new CircularListImpl();
+    private CircularListImpl circularList;
+
+    @BeforeEach
+    void beforeEach(){
+        this.circularList = new CircularListImpl();
+    }
 
     @Test
     public void initialListSize(){
@@ -20,5 +26,11 @@ public class CircularListTest {
         circularList.add(1);
         assertEquals(1, circularList.size());
     }
+
+    @Test
+    public void listIsEmptyBeforeAnyAdd(){
+        assertTrue(circularList.isEmpty());
+    }
+
 
 }
